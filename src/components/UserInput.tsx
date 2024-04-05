d98dd1e914a2e4091c471144b4e1c2f16ce34e8b
Sebastian <selfishworm117@gmail.com>
2024-04-05 11:32:24 +0200

    remove items, hover effect

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -4,28 +4,51 @@ import "./userInput.scss";
 export const UserInput = ({ children }: any) => {
   const [currentList, setCurrentList] = useState<string[]>([]);
   const inputRef = useRef<HTMLInputElement>(null);
+  const [allowAdd, setAllowAdd] = useState(true);
 
   const addTextToList = () => {
     console.log(currentList);
-    if (inputRef.current && inputRef.current.value.length > 0) {
+    if (inputRef.current && inputRef.current.value.length > 0 && currentList.length < 10) {
       setCurrentList((prevList) => [...prevList, inputRef.current!.value]);
+    } else if (currentList.length === 10) {
+      setAllowAdd(false);
     }
   };
 
   useEffect(() => {
+    if (currentList.length < 10) {
+      setAllowAdd(true);
+    }
     inputRef.current!.value = "";
+    inputRef.current?.focus();
   }, [currentList]);
+
+  const removeItem = (itemIndex: number) => {
+    setCurrentList((prevList) => prevList.filter((_, index) => index !== itemIndex));
+  };
+
   return (
     <article>
-      <input maxLength={20} type="text" name="" id="" ref={inputRef} placeholder={children} />
-      <button className="add-to-list" onClick={addTextToList}>
-        Add to list
-      </button>
+      <div className="user-interaction">
+        <div className="input-button">
+          <input maxLength={20} type="text" name="" id="" ref={inputRef} placeholder={children} />
+          <button className="add-to-list" onClick={addTextToList}>
+            Add to list
+          </button>
+        </div>
+
+        {!allowAdd ? <span>Cannot add more than 10 items</span> : <></>}
+      </div>
+
       <ul className="list">
         {currentList.length === 0 ? (
           <li>List is empty</li>
         ) : (
-          currentList.map((item, index) => <li key={index}>{item}</li>)
+          currentList.map((item, index) => (
+            <li key={index} id={`${index}`}>
+              {item} <button onClick={() => removeItem(index)}>&#10006;</button>
+            </li>
+          ))
         )}
       </ul>
     </article>
